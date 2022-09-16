61c3ff27f898b5b4bc418ab5357fc5907ac4c661
Brian Thompson (SARCAT) <brian.thompson@sarcat.io>
2022-09-16 18:59:07 -0400

    Removing comments from run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-# rm __SARCAT_ARCHIVE/bundleRegistry.sarcat.json
-# rm __SARCAT_ARCHIVE/rawScanFileRegistry.sarcat.json
-# rm __SARCAT_ARCHIVE/config.sarcat.json
-# rm -rf __SARCAT_ARCHIVE/bundles
-# mkdir __SARCAT_ARCHIVE/bundles
-# rm -rf __SARCAT_ARCHIVE/bundles/SARCAT_bundle_0001/02_assessment-data/02-02_intermediate-objects
 echo "$(pwd)" >  __SARCAT_ARCHIVE/.sarcat/.local_pwd
-docker run -it --mount type=bind,source=/Users/brianthompson/Code/SARCAT/SARCAT/__SARCAT_ARCHIVE,target=/sarcat_system/__SARCAT_ARCHIVE sarcat:1663309301 node start.mjs /Users/brianthompson/Code/SARCAT/SARCAT __SARCAT_ARCHIVE/sarcat_1663309301.tar sha256:d9e20f2df5320765657bbf8780840eea7eda4d807621b4834a7f7f33b2109353
+docker run -it --mount type=bind,source=/Users/brianthompson/Code/SARCAT/SARCAT/__SARCAT_ARCHIVE,target=/sarcat_system/__SARCAT_ARCHIVE sarcat:1663369058 node start.mjs /Users/brianthompson/Code/SARCAT/SARCAT __SARCAT_ARCHIVE/sarcat_1663369058.tar sha256:3b62cacb3c7f96d6f59a12b6f9cc389a94cb9b3eeb78e17833a48f71eadc876e
