0203e4f343e664639b99b88eededf707c335c497
Brian Thompson (SARCAT) <brian.thompson@sarcat.io>
2022-09-20 17:15:56 -0400

    removing uncompressed cleartext

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 echo "$(pwd)" >  __SARCAT_ARCHIVE/.sarcat/.local_pwd
-docker run -it --mount type=bind,source=/Users/brianthompson/Code/SARCAT/SARCAT/__SARCAT_ARCHIVE,target=/sarcat_system/__SARCAT_ARCHIVE sarcat:1663369058 node start.mjs /Users/brianthompson/Code/SARCAT/SARCAT __SARCAT_ARCHIVE/sarcat_1663369058.tar sha256:3b62cacb3c7f96d6f59a12b6f9cc389a94cb9b3eeb78e17833a48f71eadc876e
+tar -xzvf commonData/cve_data/cve.json.tar.gz commonData/cve_data/
+docker run -it --mount type=bind,source=/Users/brianthompson/Code/SARCAT/SARCAT/__SARCAT_ARCHIVE,target=/sarcat_system/__SARCAT_ARCHIVE sarcat:1663708301 node start.mjs /Users/brianthompson/Code/SARCAT/SARCAT __SARCAT_ARCHIVE/sarcat_1663708301.tar 
