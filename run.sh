44a99088f2114f167afd798fefe0c2219cf09dcd
Brian Thompson (SARCAT) <brian.thompson@sarcat.io>
2022-09-20 17:36:53 -0400

    Update shell script for CVE data

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 echo "$(pwd)" >  __SARCAT_ARCHIVE/.sarcat/.local_pwd
-tar -xzvf commonData/cve_data/cve.json.tar.gz commonData/cve_data/
-docker run -it --mount type=bind,source=/Users/brianthompson/Code/SARCAT/SARCAT/__SARCAT_ARCHIVE,target=/sarcat_system/__SARCAT_ARCHIVE sarcat:1663708301 node start.mjs /Users/brianthompson/Code/SARCAT/SARCAT __SARCAT_ARCHIVE/sarcat_1663708301.tar 
+cd $(pwd)/commonData/cve_data/
+tar -xzvf cve.json.tar.gz
+cd -
+docker run -it --mount type=bind,source=/Users/brianthompson/Code/SARCAT/SARCAT/__SARCAT_ARCHIVE,target=/sarcat_system/__SARCAT_ARCHIVE sarcat:1663709522 node start.mjs /Users/brianthompson/Code/SARCAT/SARCAT __SARCAT_ARCHIVE/sarcat_1663709522.tar 
